5b0361345e38eb0c6ed987a51f43909861d4f6e9
dimarog1 <dima.rogatnev@yandex.ru>
2024-03-22 21:45:26 +0300

    mark 9

--- server.c
+++ server.c
@@ -1,38 +1,55 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
-#include "shared_memory.h" // Убедитесь, что этот файл находится в том же каталоге, что и server.c
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "shared_memory.h"
 
-int main() {
-    // Создаем сегмент разделяемой памяти
-    int shmid = shmget(SHM_ID, sizeof(struct shared_data), 0666|IPC_CREAT);
+int shmid;
+struct shared_data* data;
+
+void handle_alarm(int sig) {
+    exit(0); // Завершение работы процесса при получении сигнала от таймера
+}
+
+int main(int argc, char *argv[]) {
+    // Создание сегмента разделяемой памяти
+    shmid = shmget(SHM_ID, sizeof(struct shared_data), 0666|IPC_CREAT);
     if (shmid == -1) {
         perror("Не удалось создать разделяемую память");
         return -1;
     }
 
-    // Присоединяем сегмент разделяемой памяти к адресному пространству процесса
-    struct shared_data* data = (struct shared_data*) shmat(shmid, NULL, 0);
+    // Прикрепление разделяемой памяти к адресному пространству процесса
+    data = (struct shared_data*) shmat(shmid, NULL, 0);
     if (data == (void*) -1) {
         perror("Shmat не удался");
         return -1;
     }
 
-    // В бесконечном цикле проверяем наличие новых данных и выводим их
+    // Установка таймера, если был передан соответствующий аргумент командной строки
+    if (argc > 1 && strcmp(argv[1], "timer") == 0) {
+        signal(SIGALRM, handle_alarm);
+        alarm(10);
+    }
+
+    // Чтение чисел из разделяемой памяти, пока клиент не завершит работу
     while (!data->is_finished) {
         if (data->is_new_data) {
-            printf("Получено число: %d\n", data->number);
-            data->is_new_data = 0;
+            printf("Получено число: %d\n", data->number); // Вывод числа
+            data->is_new_data = 0; // Сброс флага, что данные прочитаны
         }
     }
 
-    // Отсоединяем сегмент разделяемой памяти от адресного пространства процесса
+    // Отсоединение от разделяемой памяти
     if (shmdt(data) == -1) {
         perror("Shmdt не удался");
         return -1;
     }
 
-    // Удаляем сегмент разделяемой памяти
+    // Удаление сегмента разделяемой памяти
     if (shmctl(shmid, IPC_RMID, NULL) == -1) {
         perror("Shmctl не удался");
         return -1;
